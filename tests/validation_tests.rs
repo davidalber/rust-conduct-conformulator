322133ec49abeae275a879632052a0732e7e4be2
David Alber <alber.david@gmail.com>
2018-01-24 23:11:03 -0800

    Moving logic to integration test file

--- tests/validation_tests.rs
+++ tests/validation_tests.rs
@@ -1,9 +1,17 @@
 extern crate code_of_conduct_conformulator;
-
-use code_of_conduct_conformulator::CodeOfConduct;
+extern crate reqwest;
 
 use std::fs::File;
 use std::io::Read;
+
+const RUST_WWW_CODE_OF_CONDUCT: &str =
+    "https://raw.githubusercontent.com/rust-lang/rust-www/master/en-US/conduct.md";
+
+fn fetch(url: &str) -> String {
+    let mut resp = reqwest::get(url).unwrap();
+    assert!(resp.status().is_success());
+    resp.text().unwrap()
+}
 
 fn read_expected(filename: &str) -> String {
     let mut f = File::open(filename).expect("file not found");
@@ -16,7 +24,14 @@ fn read_expected(filename: &str) -> String {
 
 #[test]
 fn validate_base_file() {
-    let base = CodeOfConduct::get_base_code_of_conduct().fetch();
+    let base = fetch(RUST_WWW_CODE_OF_CONDUCT);
     let expected_base = read_expected("tests/expected/base.md");
-    assert_eq!(base, expected_base);
+
+    assert!(
+        expected_base == base,
+        format!(
+            "Rust WWW code of conduct ({}) does not match expected value",
+            RUST_WWW_CODE_OF_CONDUCT
+        )
+    );
 }
