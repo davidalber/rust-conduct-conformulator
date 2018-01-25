22bb6fffe7ed91a252990236d5af24ce122e6929
David Alber <alber.david@gmail.com>
2018-01-25 00:17:36 -0800

    Adding satellite code of conduct test and reorganizing

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,33 @@
+extern crate reqwest;
 
+#[macro_use]
+extern crate lazy_static;
+
+use std::fs::File;
+use std::io::Read;
+
+pub const RUST_WWW_CODE_OF_CONDUCT: &str =
+    "https://raw.githubusercontent.com/rust-lang/rust-www/master/en-US/conduct.md";
+
+lazy_static! {
+    pub static ref BASE: String = fetch(RUST_WWW_CODE_OF_CONDUCT);
+}
+
+pub fn fetch(url: &str) -> String {
+    let mut resp = reqwest::get(url).unwrap();
+    assert!(resp.status().is_success());
+    resp.text().unwrap()
+}
+
+pub fn read_expected(filename: &str) -> String {
+    let mut f = File::open(filename).expect("file not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+    contents
+}
+
+pub fn make_expected_satellite() -> String {
+    String::from("foo")
+}
--- tests/validation_tests.rs
+++ tests/validation_tests.rs
@@ -1,37 +1,52 @@
 extern crate code_of_conduct_conformulator;
-extern crate reqwest;
 
-use std::fs::File;
-use std::io::Read;
+#[macro_use]
+extern crate lazy_static;
 
-const RUST_WWW_CODE_OF_CONDUCT: &str =
-    "https://raw.githubusercontent.com/rust-lang/rust-www/master/en-US/conduct.md";
+use code_of_conduct_conformulator::{fetch, make_expected_satellite, read_expected, BASE,
+                                    RUST_WWW_CODE_OF_CONDUCT};
 
-fn fetch(url: &str) -> String {
-    let mut resp = reqwest::get(url).unwrap();
-    assert!(resp.status().is_success());
-    resp.text().unwrap()
-}
-
-fn read_expected(filename: &str) -> String {
-    let mut f = File::open(filename).expect("file not found");
-
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading the file");
-    contents
+lazy_static! {
+    static ref EXPECTED_SATELLITE: String = make_expected_satellite();
 }
 
 #[test]
 fn validate_base_file() {
-    let base = fetch(RUST_WWW_CODE_OF_CONDUCT);
     let expected_base = read_expected("tests/expected/base.md");
 
     assert!(
-        expected_base == base,
+        expected_base == *BASE,
         format!(
             "Rust WWW code of conduct ({}) does not match expected value",
             RUST_WWW_CODE_OF_CONDUCT
         )
+    );
+}
+
+fn satellite_matches_expected(url: &str) -> bool {
+    let satellite = fetch(url);
+    if satellite != *EXPECTED_SATELLITE {
+        println!(
+            "Satellite code of conduct ({}) does not match expected value",
+            url
+        );
+        return false;
+    }
+
+    true
+}
+
+#[test]
+fn validate_satellite_files() {
+    let urls = vec![
+        "https://raw.githubusercontent.com/rust-lang/rust/master/CODE_OF_CONDUCT.md",
+    ];
+
+    let results: Vec<bool> = urls.iter()
+        .map(|&u| satellite_matches_expected(u))
+        .collect();
+    assert!(
+        results.iter().fold(true, |acc, &b| acc && b),
+        "Satellite checks failed"
     );
 }
