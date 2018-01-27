719d1c5d542e9138a642bc05aa6261128610f173
David Alber <alber.david@gmail.com>
2018-01-27 12:17:02 -0800

    Moving function back to integration test file

--- tests/validation_tests.rs
+++ tests/validation_tests.rs
@@ -3,11 +3,22 @@ extern crate code_of_conduct_conformulator;
 #[macro_use]
 extern crate lazy_static;
 
-use code_of_conduct_conformulator::{fetch, make_expected_satellite, read_expected, BASE,
-                                    RUST_WWW_CODE_OF_CONDUCT};
+use code_of_conduct_conformulator::{fetch, make_expected_satellite, BASE, RUST_WWW_CODE_OF_CONDUCT};
+
+use std::fs::File;
+use std::io::Read;
 
 lazy_static! {
     static ref EXPECTED_SATELLITE: String = make_expected_satellite();
+}
+
+fn read_expected(filename: &str) -> String {
+    let mut f = File::open(filename).expect("file not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+    contents
 }
 
 #[test]
