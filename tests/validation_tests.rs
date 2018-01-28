a1907ae102e814ff33c8ee4a832bc4658709cc33
David Alber <alber.david@gmail.com>
2018-01-28 01:05:48 -0800

    Printing failing satellite conduct URLs

--- tests/validation_tests.rs
+++ tests/validation_tests.rs
@@ -34,30 +34,19 @@ fn validate_base_file() {
     );
 }
 
-fn satellite_matches_expected(url: &str) -> bool {
-    let satellite = fetch(url);
-    if satellite != *EXPECTED_SATELLITE {
-        println!(
-            "Satellite code of conduct ({}) does not match expected value",
-            url
-        );
-        return false;
-    }
-
-    true
-}
-
 #[test]
 fn validate_satellite_files() {
     let urls = vec![
         "https://raw.githubusercontent.com/rust-lang/rust/master/CODE_OF_CONDUCT.md",
     ];
 
-    let results: Vec<bool> = urls.iter()
-        .map(|&u| satellite_matches_expected(u))
+    let failing_urls: Vec<&str> = urls.iter()
+        .filter(|u| fetch(u) != *EXPECTED_SATELLITE)
+        .map(|u| *u)
         .collect();
+
     assert!(
-        results.iter().fold(true, |acc, &b| acc && b),
-        "Satellite checks failed"
+        failing_urls.len() == 0,
+        format!("Satellite checks failed for {}", failing_urls.join(", "))
     );
 }
