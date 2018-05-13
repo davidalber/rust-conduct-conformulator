c96eb3c3c8078c14e7034e047de470539699ecab
David Alber <alber.david@gmail.com>
2018-05-12 17:01:16 -0700

    Build conformance report for all Rust repositories and arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,35 @@
 extern crate code_of_conduct_conformulator;
+extern crate serde_json;
 
-use code_of_conduct_conformulator::make_expected_satellite;
+use code_of_conduct_conformulator::{check_repository_conformance, get_org_repositories,
+                                    make_expected_satellite};
+use serde_json::Error;
+use std::env;
 
+pub const ORGANIZATIONS: [&str; 2] = ["rust-lang", "rust-lang-nursery"];
 
-fn main() {
-    print!("{}", make_expected_satellite());
+fn get_all_repos() -> Vec<String> {
+    let mut repos: Vec<String> = Vec::new();
+    for org in ORGANIZATIONS.iter() {
+        repos.append(&mut get_org_repositories(org));
+    }
+
+    repos
+}
+
+fn main() -> Result<(), Error> {
+    let args: Vec<String> = env::args().skip(1).collect();
+    if args.len() > 0 {
+        match args[0].as_ref() {
+            "conduct" => print!("{}", make_expected_satellite()),
+            "conformance" => {
+                let repos = get_all_repos();
+                let c = check_repository_conformance(&repos);
+                println!("{}", serde_json::to_string(&c)?);
+            }
+            _ => eprintln!("Task argument (conduct | conformance) required."),
+        }
+    }
+
+    Ok(())
 }
