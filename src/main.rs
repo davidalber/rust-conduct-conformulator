4064af34ebe0ff71dcb3a9b95bed5a6c335e8526
David Alber <alber.david@gmail.com>
2018-05-14 21:17:42 -0700

    Change interfact to web app

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
 extern crate code_of_conduct_conformulator;
+extern crate rocket;
+extern crate rocket_contrib;
 extern crate serde_json;
 
 use code_of_conduct_conformulator::{check_repository_conformance, get_org_repositories,
-                                    make_expected_satellite};
-use serde_json::Error;
-use std::env;
+                                    make_expected_satellite, ConformanceReport};
+use rocket_contrib::Json;
 
 pub const ORGANIZATIONS: [&str; 2] = ["rust-lang", "rust-lang-nursery"];
 
@@ -17,21 +21,19 @@ fn get_all_repos() -> Vec<String> {
     repos
 }
 
-fn main() -> Result<(), Error> {
-    let args: Vec<String> = env::args().skip(1).collect();
-    if args.len() > 0 {
-        match args[0].as_ref() {
-            "conduct" => print!("{}", make_expected_satellite()),
-            "conformance" => {
-                let repos = get_all_repos();
-                let c = check_repository_conformance(&repos);
-                println!("{}", serde_json::to_string(&c)?);
-            }
-            _ => eprintln!("Task argument (conduct | conformance) required."),
-        }
-    } else {
-        eprintln!("Task argument (conduct | conformance) required.");
-    }
+#[get("/conduct")]
+fn conduct() -> String {
+    make_expected_satellite()
+}
 
-    Ok(())
+#[get("/conformance")]
+fn conformance() -> Json<ConformanceReport> {
+    let repos = get_all_repos();
+    Json(check_repository_conformance(&repos))
+}
+
+fn main() {
+    rocket::ignite()
+        .mount("/", routes![conduct, conformance])
+        .launch();
 }
